12076dc1d94d08d371619d449c7498dbb777af28
larrychina <larry_int@163.com>
2018-05-18 23:32:57 +0800

    init read properties

--- lcm-core/src/main/java/org/larrychina/lcm/client/LcmClentImpl.java
+++ lcm-core/src/main/java/org/larrychina/lcm/client/LcmClentImpl.java
@@ -1,5 +1,10 @@
 package org.larrychina.lcm.client;
 
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.RetrySleeper;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.larrychina.lcm.conf.LcmConfigration;
 
 import java.util.Map;
@@ -13,6 +18,10 @@ public class LcmClentImpl implements LcmClient {
     private Map<String,LcmClient> clientMap = new ConcurrentHashMap() ;
 
     protected LcmClentImpl(LcmConfigration configration){
+        // 重试策略，超时时间1000ms,重试次数为10次
+        RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000,10) ;
+        CuratorFramework client = CuratorFrameworkFactory.newClient(configration.getZkServer(), retryPolicy) ;
+        client.start();
 
     }
     @Override
